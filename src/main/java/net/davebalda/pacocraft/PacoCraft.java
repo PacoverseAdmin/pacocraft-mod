c753744646d15bcfabdc1895cdc98d5b9ffecffe
DaveBalda <davide.baldacini@gmail.com>
2024-04-12 14:18:30 +0200

    Changes to "ModItems.java"

--- src/main/java/net/davebalda/pacocraft/PacoCraft.java
+++ src/main/java/net/davebalda/pacocraft/PacoCraft.java
@@ -1,6 +1,9 @@
 package net.davebalda.pacocraft;
 
 import com.mojang.logging.LogUtils;
+import net.davebalda.pacocraft.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -26,6 +29,9 @@ public class PacoCraft
 
     public PacoCraft() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
@@ -37,7 +43,9 @@ public class PacoCraft
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ETNITE_GEM);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
