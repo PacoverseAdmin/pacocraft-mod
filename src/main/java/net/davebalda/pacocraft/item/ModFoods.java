54fe1ed22dd130384f011960d6b15d3e83ed9359
DaveBalda <davide.baldacini@gmail.com>
2024-04-14 14:32:47 +0200

    Added a recipe for the "Protein Shake"

--- src/main/java/net/davebalda/pacocraft/item/ModFoods.java
+++ src/main/java/net/davebalda/pacocraft/item/ModFoods.java
@@ -7,6 +7,8 @@ import net.minecraft.world.food.FoodProperties;
 public class ModFoods {
     public static final FoodProperties PROTEIN_SHAKE = new FoodProperties.Builder().nutrition(2)
             .saturationMod(0.2f).effect(
-                    () -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 200, 0), 1.0f
-            ).build();
+                    () -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 600, 0), 1.0f
+            ).effect(
+                    () -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, 600, 0), 1.0f
+            ).alwaysEat().build();
 }
